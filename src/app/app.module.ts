3d67d55f971737c9c962d85be15e3228878bb83d
valmatr <valmatr@gmail.com>
2020-03-13 16:23:04 +0300

    Add feed-item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeedItemComponent } from './feed-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/feed-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'feed-item',
+  templateUrl: './feed-item.component.html',
+  styleUrls: ['./feed-item.component.css']
+})
+export class FeedItemComponent {
+  title = 'feed';
+}
